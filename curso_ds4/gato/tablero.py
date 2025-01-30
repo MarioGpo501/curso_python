c383cccc9002dbfc77b7c859ecff9601b1b38041
MarioGpo501 <mariomtzg15@gmail.com>
2025-01-30 14:35:18 -0700

    Agregué la estrategia del usuario()

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -25,11 +25,29 @@ def ia(simbolos:dict):
             simbolos[x] = 'O'
             ocupado = False
 
+def usuario(simbolos:dict):
+    '''Estrategia del usuario'''
+    ocupado = True 
+    lista_numeros = [str(i) for i in range(1,10)]
+    while ocupado is True:
+        x = input('Elija un numero del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X', 'O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else: 
+                print('Esa casilla ya está ocupada')
+        else: 
+            print('Eliga un numero del 1 al 9')        
+
+
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
 
     ''' x = random.choice(numeros)
