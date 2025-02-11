8c57445cd5684894bbd02b2777bf4c4c37c70fbe
MarioGpo501 <mariomtzg15@gmail.com>
2025-02-11 14:26:39 -0700

    Primera parte

--- curso_ds4/ahorcado/funciones.py
+++ curso_ds4/ahorcado/funciones.py
@@ -7,7 +7,27 @@ def carga_archivo_texto(archivo:str)->list:
         oraciones = file.readlines()
     return oraciones
 
+def carga_plantillas(nombre_plantilla:str)->dict:
+    """ carga las plantillas del juego apartir de un archivo de texto """
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    """ despliega la plantilla del nivel seleccionado """
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+    return diccionario[nivel]
+
 if __name__ == '__main__':
-    lista = carga_archivo_texto('./plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    despliega_plantilla(plantillas, 4)
+    despliega_plantilla(plantillas, 3)
+    despliega_plantilla(plantillas, 2)
+    despliega_plantilla(plantillas, 1)
+    despliega_plantilla(plantillas, 0)
+    despliega_plantilla(plantillas, 6)
