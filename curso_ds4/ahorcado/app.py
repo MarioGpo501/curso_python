ee568f4dd816f043b645f3d579f2d187f8dabdd6
MarioGpo501 <mariomtzg15@gmail.com>
2025-02-17 14:48:48 -0700

    RESULTADO FINAL AHORCADO

--- curso_ds4/ahorcado/app.py
+++ curso_ds4/ahorcado/app.py
@@ -1,33 +1,45 @@
-""" 
-    Programa  principal del juego del ahorcado
-"""
-import string 
+'''
+Programa principal del juego del ahorcado
+'''
+import os
+import string
 import unicodedata
+import argparse
 from random import choice
 import funciones as fn
 
-def main(archivo_texto:str, nombre_plantilla="plantilla"):
-   """ Programa principal del juego del ahorcado 
-   """
-
+def main(archivo_texto:str, nombre_plantilla='plantilla'):
+    '''
+    Programa principal del juego del ahorcado
+    '''
     # cargamos las plantillas
-    
     plantillas = fn.carga_plantillas(nombre_plantilla)
+    # cargamos las oraciones
     oraciones = fn.carga_archivo_texto(archivo_texto)
-    #obtenemos las palabras del archivo de texto
+    # obtenemos las palabras
     palabras = fn.obten_palabras(oraciones)
-    o = 5 # oportunidades a fallar 
-    p = choice(palabras) # palabra a adivinar
-    abcdarios = {letra:letra for letra in string.ascci_lowercase}
-    adivinadas = set()    
-    while o > 0:
+    o = 5 # 5 oportunidades de fallar
+    p = choice(palabras) # elegimos una palabra al azar
+    abcdario = {letra:letra for letra in string.ascii_lowercase}
+    adivinadas = set()
+    while o>0:
         fn.despliega_plantilla(plantillas, o)
-        fn.adivina_letra(abcdarios,p,adivinadas,o)
+        o = fn.adivina_letra(abcdario, p, adivinadas, o)
         if p == ''.join([letra if letra in adivinadas else '_' for letra in p]):
             print('Felicidades, adivinaste la palabra')
             break
+    fn.despliega_plantilla(plantillas, o)
+    print(f"La palabra era: {p}")
 
 if __name__ == '__main__':
-    archivo = './datos/pg15532.txt'
-    main(archivo)
+    parser = argparse.ArgumentParser(description='Juego del ahorcado')
+    parser.add_argument('-a','--archivo', help='Archivo de texto con palabras a adivinar', default = '.datos/pg15532.txt')
+    args = parser.parse_args()
+    archivo = args.archivo
+    #os.chdir('./curso_ds4/ahorcado')
+    if os.stat(archivo) is False:
+        print(f'El archivo {archivo} no existe')
+    else: 
+        main(archivo)
+
 
